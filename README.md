b5f984e56408910fdb025b557f67bab10f84d06f
liming <liming@wuzangcloud.com>
2020-01-20 20:59:17 +0800

    :fire:

--- README.md
+++ README.md
@@ -23,3 +23,32 @@ nextjs不支持css文件引入
 antd 按需加载
 > yarn add antd babel-plugin-import
 配置.babelrc文件
+
+#### nextjs 
+- 路由调转
+1. import Link from 'next/link'
+<Link href='/a' ></Link>
+Link 会渲染成button，监听点击事件，路由跳转
+
+2. Router模块
+import Router from 'next/router'
+Router.push('/a')
+
+**动态路由**
+只有query，没有params
+<Link href='/a?id=1' ></Link>
+
+**路由映射**
+link上的as 属性
+<Link href='/a?id=1' as='/a/1'></Link>
+Router.push({
+    pathname: '/a',
+    query: {
+        id: 1
+    }
+}, '/a/1')
+
+bug: 刷新会报404
+解决：在服务端解决
+
+**Router 钩子**
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Koa = require('koa')
 const next = require('next')
+const Router = require('next/router')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -8,8 +9,23 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(()=>{
     const server = new Koa()
+    const router = new Router()
+
+    // 处理 路由映射 浏览器刷新 导致 404的bug
+    router.get('a/:id', async(ctx) => {
+        const id = ctx.params.id
+        await handle(ctx.req, ctx.res, {
+            pathname: '/a',
+            query: {id}
+        })
+        ctx.respond = false
+    })
+    server.use(router.routes())
 
     server.use(async (ctx, next) => {
+        // ctx.req 是node的 request 对象
+        //  ctx.request是koa的 request对象
+        // 这里是nextjs为了兼容多中node框架，所有采用的是node的对象
         await handle(ctx.req, ctx.res)
         ctx.respond = false
     })
